c8b36fe4f0bb50f1043ecd1b933dd2472468a314
IBITOYEVICTOR001 <victoribitoye70@gmail.com>
2026-08-07 11:39:05 +0100

    Include public assets in API build

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -3,7 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { build as esbuild } from "esbuild";
 import esbuildPluginPino from "esbuild-plugin-pino";
-import { rm } from "node:fs/promises";
+import { cp, rm } from "node:fs/promises";
 
 // Plugins (e.g. 'esbuild-plugin-pino') may use `require` to resolve dependencies
 globalThis.require = createRequire(import.meta.url);
@@ -111,7 +111,7 @@ async function buildAll() {
     sourcemap: "linked",
     plugins: [
       // pino relies on workers to handle logging, instead of externalizing it we use a plugin to handle it
-      esbuildPluginPino({ transports: ["pino-pretty"] })
+      esbuildPluginPino({ transports: ["pino-pretty"] }),
     ],
     // Make sure packages that are cjs only (e.g. express) but are bundled continue to work in our esm output file
     banner: {
@@ -125,6 +125,14 @@ globalThis.__dirname = __bannerPath.dirname(globalThis.__filename);
     `,
     },
   });
+
+  await cp(
+    path.resolve(artifactDir, "public"),
+    path.resolve(distDir, "public"),
+    {
+      recursive: true,
+    },
+  );
 }
 
 buildAll().catch((err) => {
